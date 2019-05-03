ecfaeeaa26e002aa027017a2d73388dfa30f64d4
huyaqin666 <1184173192@qq.com>
2019-05-03 21:37:18 +0800

    SJCHJSD

--- app/src/main/java/com/example/huyaqin/myapplication123/MainActivity.java
+++ app/src/main/java/com/example/huyaqin/myapplication123/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.huyaqin.myapplication123;
+﻿package com.example.huyaqin.myapplication123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
     //呼呼呼呼呼
     //建行股份实际的官方数据的粉红色
     //oooooooo
+//KWJHDKWJDHW
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
