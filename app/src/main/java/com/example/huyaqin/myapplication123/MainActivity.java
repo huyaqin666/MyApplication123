9bbd7fb8dfe47d7a8e4165ee4ee7cb030c7a7be2
huyaqin666 <1184173192@qq.com>
2019-05-03 21:20:54 +0800

    呼雅琴提交的

--- app/src/main/java/com/example/huyaqin/myapplication123/MainActivity.java
+++ app/src/main/java/com/example/huyaqin/myapplication123/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//呼呼呼呼呼
+    //呼呼呼呼呼
+    //建行股份实际的官方数据的粉红色
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
